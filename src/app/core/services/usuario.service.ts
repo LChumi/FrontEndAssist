d5c0e2a0eaa76e805db034341206e01f63aec4f3
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2024-11-27 15:29:10 -0500

    feat(Adding service to user )

--- src/app/core/services/usuario.service.ts
+++ src/app/core/services/usuario.service.ts
@@ -3,19 +3,25 @@ import {environment} from "@environments/environment";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {ServiceResponse} from "@models/record/service-response";
+import {AuthenticationRequest} from "@models/auth/authentication-request";
+import {UserResponse} from "@models/record/user-response";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
-  private url = environment.apiUrlBase+'assist'
+  private baseUrl = environment.apiUrlBase+'assist'
   http = inject(HttpClient)
 
   constructor() { }
 
+  temporalLogin(request: AuthenticationRequest): Observable<UserResponse>{
+    return this.http.post<UserResponse>(`${this.baseUrl}/login`,request)
+  }
+
   recoveryPassword(userId: string): Observable<ServiceResponse>{
-    return this.http.get<ServiceResponse>(`${this.url}/usuario/${userId}`)
+    return this.http.get<ServiceResponse>(`${this.baseUrl}/forgot-password/${userId}`)
   }
 
 }
