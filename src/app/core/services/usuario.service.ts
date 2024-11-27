c96b2aa92090eb1ecc73f4b4759ca83ebf768fa8
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2024-11-27 10:30:50 -0500

    style(change styles in login html)

---
+++ src/app/core/services/usuario.service.ts
@@ -0,0 +1,21 @@
+import {inject, Injectable} from '@angular/core';
+import {environment} from "@environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {ServiceResponse} from "@models/service-response";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  private url = environment.apiUrlBase+'assist'
+  http = inject(HttpClient)
+
+  constructor() { }
+
+  recoveryPassword(userId: string): Observable<ServiceResponse>{
+    return this.http.get<ServiceResponse>(`${this.url}/usuario/${userId}`)
+  }
+
+}
