a933da0b5d1f679eb3d6478409f1c258feaafc9b
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2024-11-27 17:31:12 -0500

    feat(update service- response and add password recovery)
    -Modified service-response
    
    -Added backend service consumption for communication
    
    -Implementes password recovery function
    
    Update view for handling success and error responses

--- src/app/features/auth/forgotpassword/forgotpassword.component.ts
+++ src/app/features/auth/forgotpassword/forgotpassword.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {InputTextModule} from "primeng/inputtext";
 import {ButtonDirective} from "primeng/button";
 import {Ripple} from "primeng/ripple";
 import {RouterLink} from "@angular/router";
 import {ConfigComponent} from "@layout/config/config.component";
+import {ServiceResponse} from "@models/record/service-response";
+import {UsuarioService} from "@services/usuario.service";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   standalone: true,
@@ -12,11 +15,42 @@ import {ConfigComponent} from "@layout/config/config.component";
     ButtonDirective,
     Ripple,
     RouterLink,
-    ConfigComponent
+    ConfigComponent,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './forgotpassword.component.html',
   styles: ``
 })
-export default class ForgotpasswordComponent {
+export default class ForgotpasswordComponent implements OnInit{
+
+  resendForm!: FormGroup
+
+  response= true;
+  submitted = false;
+  serviceResponse: ServiceResponse={} as ServiceResponse
+
+  usuarioService = inject(UsuarioService)
+  fb = inject(FormBuilder)
+
+  ngOnInit(): void {
+    this.resendForm = this.fb.group({
+      usuario: ['', Validators.required]
+    })
+  }
+
+  onSubmit(){
+    this.submitted= true;
+    if (this.resendForm.invalid){
+      return
+    }
+    const usuario = this.resendForm.get('usuario')?.value;
+    this.usuarioService.recoveryPassword(usuario).subscribe(
+      data=>{
+        this.response=false
+        this.serviceResponse = data
+      }
+    )
+  }
 
 }
