1e593f80e75334f4aefa243592647154eb438b4d
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2024-12-03 18:35:00 -0500

    feat:(Add company list on login and update Angular routes and navigation to connect services)

--- src/app/features/empresa/empresa.component.ts
+++ src/app/features/empresa/empresa.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {Empresa} from "@models/entities/empresa";
+import {MenusService} from "@services/menus.service";
+import {ButtonDirective} from "primeng/button";
+import {Ripple} from "primeng/ripple";
+import {ConfigComponent} from "@layout/config/config.component";
+import {Router} from "@angular/router";
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonDirective,
+    Ripple,
+    ConfigComponent
+  ],
   templateUrl: './empresa.component.html',
   styles: ``
 })
-export class EmpresaComponent {
+export default class EmpresaComponent implements OnInit{
 
+  menuService= inject(MenusService)
+  router = inject(Router)
+
+  listasEmpresa: Empresa[] =[]
+
+  ngOnInit(): void {
+    const usrIdString =sessionStorage.getItem('usrid')
+    if (usrIdString){
+      const usrId = Number(usrIdString)
+      this.menuService.getEmpresas(usrId).subscribe(
+        empresas => {
+          this.listasEmpresa=empresas
+        }
+      )
+    }
+  }
+
+  empresaSelected(empresa:Empresa){
+    if (empresa){
+      sessionStorage.setItem('empresa', String(empresa.id))
+      this.goToInicio()
+    }
+  }
+
+  goToInicio(){
+    this.router.navigate(['/assist', 'inicio'])
+  }
 }
