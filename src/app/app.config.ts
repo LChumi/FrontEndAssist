da313f91ad11f5ad003e334f68dd1d1840995b4d
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2024-12-05 19:36:31 -0500

    feat(upload): Add document upload view, service, and toast notifications. Includes error handling for invalid file types

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {provideAnimations} from "@angular/platform-browser/animations";
 import {provideHttpClient, withFetch, withInterceptors} from "@angular/common/http";
 import {LayoutComponent} from "./layout/components/layout/layout.component";
+import {MessageService} from "primeng/api";
+import {ToastModule} from "primeng/toast";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    MessageService,
+    importProvidersFrom(ToastModule),
     provideHttpClient(withFetch(), withInterceptors([]))
   ]
 };
