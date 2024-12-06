cfb582af957f05645e66000ef891ccc2e0ca9a31
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2024-12-06 11:53:10 -0500

    feat(Add styles and atributes to html component)

--- src/app/features/importacion/procesos/carga-solicitud/carga-solicitud.component.ts
+++ src/app/features/importacion/procesos/carga-solicitud/carga-solicitud.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FileUploadModule} from "primeng/fileupload";
+import {MessageService, PrimeTemplate} from "primeng/api";
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [
+    FileUploadModule,
+    PrimeTemplate
+  ],
   templateUrl: './carga-solicitud.component.html',
   styles: ``
 })
 export default class CargaSolicitudComponent {
+
+  uploadFiles: any[] = []; // Archivos seleccionados
+  messageService = inject(MessageService);
+
+
+  onUpload(event : any) {
+    for(let file of event.files) {
+      this.uploadFiles.push(file);
+    }
+
+    this.messageService.add({severity: 'info', summary: 'File Uploaded', detail: ''});
+  }
 
 }
