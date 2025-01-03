ac01b144702e942d0c0f5f669b6eec33e5af39c1
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-01-03 14:30:09 -0500

    feat added service to favorite create new interfaces and update service

--- src/app/core/services/state/favorites.service.ts
+++ src/app/core/services/state/favorites.service.ts
@@ -1,28 +1,34 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {environment} from "@environments/environment";
+import {FavoriteRequest} from "@models/record/favorite-request";
+import {Observable} from "rxjs";
+import {UsuarioFavoritos} from "@models/entities/usuario-favoritos";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavoritesService {
 
+  private baseUrl = environment.apiUrlBase+ 'assist'
+
   http = inject(HttpClient)
 
   constructor() { }
 
-  addFavorite(usrId: number, path:string) {
-
+  addFavorite(request: FavoriteRequest): Observable<UsuarioFavoritos> {
+    return this.http.post<UsuarioFavoritos>(`${this.baseUrl}/favoritos/add`, request)
   }
 
   deleteFavorite(usrId: number, path:string) {
 
   }
 
-  getFavorites(usrId: number) {
-
+  getFavorites(usrId: number, empresa: number): Observable<UsuarioFavoritos[]>{
+    return this.http.get<UsuarioFavoritos[]>(`${this.baseUrl}/favoritos/${usrId}/${empresa}`)
   }
 
-  isFavorited(usrId: number, path:string) {
-
+  isFavorited(request: FavoriteRequest): Observable<boolean> {
+    return this.http.post<boolean>(`${this.baseUrl}/favoritos/get`, request)
   }
 }
