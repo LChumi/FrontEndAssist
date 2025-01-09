5f638f451d9708bd688d9e3e080238f38b535b2e
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-01-09 17:37:49 -0500

    feat:(added new component modal )

---
+++ src/app/features/shared/component/seleccion-comprobante/seleccion-comprobante.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {DialogModule} from "primeng/dialog";
+import {ButtonDirective} from "primeng/button";
+import {DropdownModule} from "primeng/dropdown";
+import {ChipsModule} from "primeng/chips";
+
+@Component({
+  selector: 'app-seleccion-comprobante',
+  standalone: true,
+  imports: [
+    DialogModule,
+    ButtonDirective,
+    DropdownModule,
+    ChipsModule
+  ],
+  templateUrl: './seleccion-comprobante.component.html',
+  styles: ``
+})
+export class SeleccionComprobanteComponent {
+
+  visible = true;
+}
