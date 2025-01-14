bc27993e4d914284ca4212071daa984c3aa83674
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-01-14 17:46:54 -0500

    feat(added new interfaces and services to them )

---
+++ src/app/core/services/api/almacen.service.ts
@@ -0,0 +1,20 @@
+import {inject, Injectable} from '@angular/core';
+import {environment} from "@environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Almacen} from "@models/entities/almacen";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlmacenService {
+
+  private baseUrl = environment.apiUrlBase + 'models';
+  private http = inject(HttpClient)
+
+  constructor() { }
+
+  getAlmacen(empresa:number, codigo: number): Observable<Almacen> {
+    return this.http.get<Almacen>(`${this.baseUrl}/almacen-get/${empresa}/${empresa}`)
+  }
+}
