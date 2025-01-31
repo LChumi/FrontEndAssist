c5a288532d6cd311f1e2466d2e993db1418d8f65
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-01-31 18:40:31 -0500

    style: create style to consultasComponent

--- src/app/features/importacion/consultas/consultas-importacion/consultas-importacion.component.ts
+++ src/app/features/importacion/consultas/consultas-importacion/consultas-importacion.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import {InputTextModule} from "primeng/inputtext";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [
+    InputTextModule,
+    FormsModule
+  ],
   templateUrl: './consultas-importacion.component.html',
   styles: ``
 })
 export class ConsultasImportacionComponent {
+
+  periodo:  any;
+  fecha:    any;
+  mes:      any;
+  sigla:    any;
+  almacen:  any;
+  serie:    any;
+  numero!:  number;
 
 }
