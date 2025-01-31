ba1d9f0e4ccf759e07b0652958def131dbe2c4c4
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-01-31 18:21:04 -0500

    feat: added new interfaces and service

---
+++ src/app/features/importacion/consultas/consultas-importacion/consultas-importacion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './consultas-importacion.component.html',
+  styles: ``
+})
+export class ConsultasImportacionComponent {
+
+}
