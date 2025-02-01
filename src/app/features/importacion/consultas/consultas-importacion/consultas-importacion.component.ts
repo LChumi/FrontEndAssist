c75b0f75d203ee155d21907578cf5f6a418330c1
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-02-01 10:28:44 -0500

    style: changes in inputs

--- src/app/features/importacion/consultas/consultas-importacion/consultas-importacion.component.ts
+++ src/app/features/importacion/consultas/consultas-importacion/consultas-importacion.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {InputTextModule} from "primeng/inputtext";
 import {FormsModule} from "@angular/forms";
+import {CalendarModule} from "primeng/calendar";
+import {DropdownModule} from "primeng/dropdown";
+import {Almacen} from "@models/entities/almacen";
 
 @Component({
   standalone: true,
   imports: [
     InputTextModule,
-    FormsModule
+    FormsModule,
+    CalendarModule,
+    DropdownModule
   ],
   templateUrl: './consultas-importacion.component.html',
   styles: ``
@@ -20,5 +25,13 @@ export default class ConsultasImportacionComponent {
   almacen:  any;
   serie:    any;
   numero!:  number;
+
+  almacenes: Almacen[] = [];
+
+  almacenSelected: Almacen = {} as Almacen;
+
+  onAlmacenChange(event: any) {
+    this.almacenSelected = event.value;
+  }
 
 }
