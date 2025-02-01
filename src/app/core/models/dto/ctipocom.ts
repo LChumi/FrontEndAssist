ce48756ebaeaf5ed4c370d01bff67b375f30b30a
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-02-01 10:41:09 -0500

    feat:added new service and interface

---
+++ src/app/core/models/dto/ctipocom.ts
@@ -0,0 +1,6 @@
+export interface Ctipocom {
+  codigo:  number;
+  empresa: number;
+  ctiId:   string;
+  nombre:  string;
+}
---
+++ src/app/core/services/api/ctipocom.service.ts
@@ -0,0 +1,20 @@
+import {inject, Injectable} from '@angular/core';
+import {environment} from "@environments/environment";
+import {Observable} from "rxjs";
+import {Ctipocom} from "@models/dto/ctipocom";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CtipocomService {
+
+  private baseUrl = environment.apiUrlBase + 'models';
+  private http = inject(HttpClient);
+
+  constructor() { }
+
+  listar(empresa: any): Observable<Ctipocom[]> {
+    return this.http.get<Ctipocom[]>(`${this.baseUrl}/listar/ctipocom/${empresa}`)
+  }
+}
