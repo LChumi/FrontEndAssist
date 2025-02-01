f3bac50ff24b730a80c343cb49ed050eaa2b6ea7
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-02-01 12:15:43 -0500

    refactor: rename classes and services

--- src/app/features/importacion/consultas/consultas-importacion/consultas-importacion.component.ts
+++ src/app/features/importacion/consultas/consultas-importacion/consultas-importacion.component.ts
@@ -1,9 +1,14 @@
-import {Component} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {InputTextModule} from "primeng/inputtext";
 import {FormsModule} from "@angular/forms";
 import {CalendarModule} from "primeng/calendar";
 import {DropdownModule} from "primeng/dropdown";
 import {Almacen} from "@models/entities/almacen";
+import {Ctipocom} from "@models/dto/ctipocom";
+import {AutoCompleteModule} from "primeng/autocomplete";
+import {CtipocomService} from "@services/api/ctipocom.service";
+import {AlmacenService} from "@services/api/almacen.service";
+import {getSessionItem} from "@utils/storage-utils";
 
 @Component({
   standalone: true,
@@ -11,27 +16,79 @@ import {Almacen} from "@models/entities/almacen";
     InputTextModule,
     FormsModule,
     CalendarModule,
-    DropdownModule
+    DropdownModule,
+    AutoCompleteModule
   ],
   templateUrl: './consultas-importacion.component.html',
   styles: ``
 })
-export default class ConsultasImportacionComponent {
+export default class ConsultasImportacionComponent implements OnInit {
 
-  periodo:  any;
-  fecha:    any;
-  mes:      any;
-  sigla:    any;
-  almacen:  any;
-  serie:    any;
-  numero!:  number;
+  private ctipocomService = inject(CtipocomService);
+  private almacenService = inject(AlmacenService)
 
-  almacenes: Almacen[] = [];
+  protected periodo:  any;
+  protected fecha:    any;
+  protected mes:      any;
+  protected sigla:    any;
+  protected almacen:  any;
+  protected serie:    any;
+  protected numero!:  number;
+  private   empresa:  any;
 
-  almacenSelected: Almacen = {} as Almacen;
+  protected almacenes: Almacen[] = [];
+  protected siglas: Ctipocom[] = [];
+  protected filteredCountries: Ctipocom[] = [];
+
+  protected almacenSelected: Almacen = {} as Almacen;
+  protected siglaSelectedSelected: Ctipocom = {} as Ctipocom;
+
+  ngOnInit(): void {
+    this.empresa = getSessionItem("empresa");
+    this.getAlmacenes()
+    this.getSiglas()
+  }
+
 
   onAlmacenChange(event: any) {
     this.almacenSelected = event.value;
   }
 
+  searchSigla(event: any){
+    const filtered: any[] =[]
+    const query = event.query;
+
+    for (let i = 0; i < this.siglas.length; i++) {
+      const sig = this.siglas[i];
+      if (sig.ctiId.toLowerCase().indexOf(query.toLowerCase()) == 0){
+        filtered.push(sig);
+      }
+    }
+    this.filteredCountries = filtered;
+  }
+
+  getAlmacenes() {
+    if (this.empresa) {
+      this.almacenService.listAlamacenes(this.empresa).subscribe({
+        next: (result) => {
+          this.almacenes = result;
+        }
+      })
+    }
+  }
+
+  getSiglas() {
+    if (this.empresa) {
+      this.ctipocomService.listar(this.empresa).subscribe({
+        next: (result) => {
+          this.siglas = result;
+        }
+      })
+    }
+  }
+
+  save(){
+    console.log(this.sigla);
+    console.log(this.almacenSelected);
+  }
 }
