78b9b84e31a386fd701806f8952cd145e51d3fd9
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-02-03 10:34:51 -0500

    feat: added setTranslation in AppComponent to affect more components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,5 +16,14 @@ export class AppComponent implements OnInit{
 
   ngOnInit() {
     this.primengConfig.ripple = true;
+    this.primengConfig.setTranslation({
+      dayNames: ["domingo", "lunes", "martes", "miércoles", "jueves", "viernes", "sábado"],
+      dayNamesShort: ["dom", "lun", "mar", "mié", "jue", "vie", "sáb"],
+      dayNamesMin: ["D", "L", "M", "X", "J", "V", "S"],
+      monthNames: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+      monthNamesShort: ["ene", "feb", "mar", "abr", "may", "jun", "jul", "ago", "sep", "oct", "nov", "dic"],
+      today: "Hoy",
+      clear: "Limpiar"
+    });
   }
 }
--- src/app/features/importacion/consultas/consultas-importacion/consultas-importacion.component.ts
+++ src/app/features/importacion/consultas/consultas-importacion/consultas-importacion.component.ts
@@ -27,7 +27,6 @@ export default class ConsultasImportacionComponent implements OnInit {
 
   private ctipocomService = inject(CtipocomService);
   private almacenService = inject(AlmacenService);
-  private primengConfig = inject(PrimeNGConfig)
 
   protected periodo:  any;
   protected fecha:    any;
@@ -49,15 +48,6 @@ export default class ConsultasImportacionComponent implements OnInit {
     this.empresa = getSessionItem("empresa");
     this.getAlmacenes()
     this.getSiglas()
-    this.primengConfig.setTranslation({
-      dayNames: ["domingo", "lunes", "martes", "miércoles", "jueves", "viernes", "sábado"],
-      dayNamesShort: ["dom", "lun", "mar", "mié", "jue", "vie", "sáb"],
-      dayNamesMin: ["D", "L", "M", "X", "J", "V", "S"],
-      monthNames: ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"],
-      monthNamesShort: ["ene", "feb", "mar", "abr", "may", "jun", "jul", "ago", "sep", "oct", "nov", "dic"],
-      today: "Hoy",
-      clear: "Limpiar"
-    });
   }
 
 
