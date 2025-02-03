201abc831b0564de1b7b13f2fcd693fc0e4af291
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-02-03 17:19:54 -0500

    feat:(added new interfaces and services to them )

---
+++ src/app/core/services/api/tipodoc.service.ts
@@ -0,0 +1,20 @@
+import {inject, Injectable} from '@angular/core';
+import {environment} from "@environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Tipodoc} from "@models/entities/tipodoc";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TipodocService {
+
+  private baseUrl = environment.apiUrlBase + 'models';
+  private http = inject(HttpClient);
+
+  constructor() { }
+
+  listarTipoDocs(): Observable<Tipodoc[]> {
+    return this.http.get<Tipodoc[]>(`${this.baseUrl}/listar/tipodoc`);
+  }
+}
