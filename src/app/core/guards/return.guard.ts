7b97afa009f324142bbb6b162d6aed6951fc0bf6
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-02-05 12:56:42 -0500

    style: changes in confirmation to candeactivate

--- src/app/core/guards/return.guard.ts
+++ src/app/core/guards/return.guard.ts
@@ -1,5 +1,23 @@
 import { CanDeactivateFn } from '@angular/router';
+import {inject} from "@angular/core";
+import {ConfirmationService, MessageService} from "primeng/api";
 
 export const returnGuard: CanDeactivateFn<unknown> = (component, currentRoute, currentState, nextState) => {
-  return confirm('Tienes cambios sin guardar. ¿Estás seguro de que quieres salir?');
+  const confirmationService = inject(ConfirmationService);
+  const toastService = inject(MessageService);
+
+  return new Promise<boolean>((resolve) => {
+    confirmationService.confirm({
+      message: 'Tienes cambios sin guardar. ¿Estas seguro de que quieres salir?',
+      header: 'Confirmación',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        resolve(true);
+      },
+      reject: () => {
+
+        resolve(false);
+      }
+    })
+  })
 };
