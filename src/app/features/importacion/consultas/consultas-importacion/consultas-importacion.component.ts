5cd0f1c0a185fa5cbc3d1d8b64e49f1dbc91bcdb
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-02-07 18:30:33 -0500

    feat: added filter by find

--- src/app/features/importacion/consultas/consultas-importacion/consultas-importacion.component.ts
+++ src/app/features/importacion/consultas/consultas-importacion/consultas-importacion.component.ts
@@ -1,13 +1,38 @@
 import {Component, inject, OnInit} from '@angular/core';
+import {ImpProdTrancitoService} from "@services/api/imp-prod-trancito.service";
+import {ChipsModule} from "primeng/chips";
+import {FormsModule} from "@angular/forms";
+import {CalendarModule} from "primeng/calendar";
+import {DropdownModule} from "primeng/dropdown";
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [
+    ChipsModule,
+    FormsModule,
+    CalendarModule,
+    DropdownModule
+  ],
   templateUrl: './consultas-importacion.component.html',
   styles: ``
 })
 export default class ConsultasImportacionComponent implements OnInit {
 
+  private impProdTrancitoService = inject(ImpProdTrancitoService);
+
+  protected nroComprobante!: string;
+  protected observacion!: string;
+  protected fecha: any;
+  protected estados: any
+
   ngOnInit(): void {
+    this.estados =[
+      {name: 'LIQUIDADO'},
+      {name: 'EN PROCESO'},
+      {name: 'PRELIQUIDADO PARCIAL'},
+      {name: 'ELIMINADO'},
+      {name: 'DISTR GASTOS'},
+      {name: 'RELIQUIDADO TOTAL'},
+    ]
   }
 }
