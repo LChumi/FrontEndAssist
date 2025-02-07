ee47e42587ece7eb6fad7fd9ff165e00c4081a32
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-02-07 12:53:08 -0500

    refactor:(changes in routes and components)

--- src/app/features/importacion/importaciones.routes.ts
+++ src/app/features/importacion/importaciones.routes.ts
@@ -3,10 +3,16 @@ import {returnGuard} from "@guards/return.guard";
 
 export const importacionesRoutes: Routes = [
   {
-    path: 'carga-solicitud',
-    loadComponent: () => import('./procesos/carga-solicitud/carga-solicitud.component'),
-    data: {breadcrumb: 'Carga solicitud '},
-    canDeactivate: [returnGuard]
+    path: 'procesos',
+    data: {breadcrumb: 'Procesos'},
+    children : [
+      {
+        path: 'carga-solicitud',
+        loadComponent: () => import('./procesos/carga-solicitud/carga-solicitud.component'),
+        data: {breadcrumb: 'Carga solicitud '},
+        canDeactivate: [returnGuard],
+      }
+    ]
   },
   {
     path: 'consultas',
