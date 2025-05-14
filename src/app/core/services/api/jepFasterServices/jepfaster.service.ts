d4b9e09ecbc7eb3f2051873612c09515c055600c
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-05-14 18:28:18 -0500

    feat: added services to jepFaster

--- src/app/core/services/api/jepFasterServices/jepfaster.service.ts
+++ src/app/core/services/api/jepFasterServices/jepfaster.service.ts
@@ -2,6 +2,8 @@ import {inject, Injectable} from '@angular/core';
 import {environment} from "@environments/environment";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {JepResponse} from "@models/record/jep-response";
+import {ServiceResponse} from "@models/record/service-response";
 
 @Injectable({
   providedIn: 'root'
@@ -13,16 +15,16 @@ export class JepfasterService {
 
   constructor() { }
 
-  generarQr(usrLiq: number, empresa: number):Observable<any>{
-    return this.http.get(`${this.baseUrl}/jep-faster/qr/${usrLiq}/${empresa}`)
+  generarQr(usrLiq: number, empresa: number):Observable<JepResponse>{
+    return this.http.get<JepResponse>(`${this.baseUrl}/jep-faster/qr/${usrLiq}/${empresa}`)
   }
 
-  validarPago(usrLiq: number, empresa: number):Observable<any>{
-    return this.http.get(`${this.baseUrl}/jep-faster/validar-pago/${usrLiq}/${empresa}`)
+  validarPago(usrLiq: number, empresa: number):Observable<ServiceResponse>{
+    return this.http.get<ServiceResponse>(`${this.baseUrl}/jep-faster/validar-pago/${usrLiq}/${empresa}`)
   }
 
-  verificarPago(usrLiq: number, empresa: number):Observable<any>{
-    return this.http.get(`${this.baseUrl}/jep-faster/verificar-pago/${usrLiq}/${empresa}`)
+  verificarPago(usrLiq: number, empresa: number):Observable<ServiceResponse>{
+    return this.http.get<ServiceResponse>(`${this.baseUrl}/jep-faster/verificar-pago/${usrLiq}/${empresa}`)
   }
 
 }
