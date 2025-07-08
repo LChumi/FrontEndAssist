b6701baf5b337fd70c0e841548edc58065378df5
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-07-07 19:35:38 -0500

    chore(seo): apply accessibility fixes, link optimizations and LCP improvements

--- src/app/features/privacy-policy/privacy-policy.component.ts
+++ src/app/features/privacy-policy/privacy-policy.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {Meta, Title} from "@angular/platform-browser";
 
 @Component({
   standalone: true,
@@ -6,6 +7,18 @@ import { Component } from '@angular/core';
   templateUrl: './privacy-policy.component.html',
   styles: ``
 })
-export class PrivacyPolicyComponent {
+export class PrivacyPolicyComponent implements OnInit {
+
+  titleService = inject(Title);
+  metaService = inject(Meta);
+
   protected emailInfo: string='info@cumpleanos.com.ec';
+
+  ngOnInit(): void {
+    this.titleService.setTitle('Politica de Privacidad');
+    this.metaService.updateTag({
+      name: 'description',
+      content: 'Nuestra Politica de privacidad'
+    });
+  }
 }
