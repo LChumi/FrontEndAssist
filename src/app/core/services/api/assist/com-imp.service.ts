21441048bad9611bcb04cf6401b4f64c907a27a5
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-12-26 12:29:30 -0500

    feat: added new module

---
+++ src/app/core/services/api/assist/com-imp.service.ts
@@ -0,0 +1,20 @@
+import {inject, Injectable} from '@angular/core';
+import {environment} from "@environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {ComImpV1} from "@models/view/com-imp-v1";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ComImpService {
+
+  private _url = environment.apiUrlBase + '/compimp';
+  private _http = inject(HttpClient);
+
+  constructor() { }
+
+  getImportacionPen(empresa : number): Observable<ComImpV1>{
+    return this._http.get<ComImpV1>(`${this._url}/${empresa}`)
+  }
+}
