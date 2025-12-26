7ddf7493e2e9c01f2ac1dd0ed884a7c793b6f5d6
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-12-26 18:27:46 -0500

    feat: added styles and table of list by docs and added services to use them

--- src/app/features/importacion/procesos/carga-importacion/carga-importacion.component.ts
+++ src/app/features/importacion/procesos/carga-importacion/carga-importacion.component.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {ComImpService} from "@services/api/assist/com-imp.service";
+import {SeoHelperService} from "@services/state/seo-helper.service";
+import {ClarityService} from "@services/state/clarity.service";
+import {ComImpV1} from "@models/view/com-imp-v1";
+import {FavoriteComponent} from "@shared/component/favorite/favorite.component";
+import {getSessionItem} from "@utils/storage-utils";
+import {Table, TableModule} from "primeng/table";
+import {InputTextModule} from "primeng/inputtext";
+import {ButtonDirective} from "primeng/button";
+import {DatePipe} from "@angular/common";
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [
+    FavoriteComponent,
+    TableModule,
+    InputTextModule,
+    ButtonDirective,
+    DatePipe
+  ],
   templateUrl: './carga-importacion.component.html',
   styles: ``
 })
-export class CargaImportacionComponent {
+export default class CargaImportacionComponent implements OnInit {
 
+  private comimpService = inject(ComImpService)
+  private seoHelper = inject(SeoHelperService);
+  private clarity= inject(ClarityService)
+
+  listImportaciones: ComImpV1[] =[]
+  docSelected: ComImpV1 | null = null;
+  imporSelected = false;
+
+  ngOnInit() {
+    const emp =getSessionItem('empresa')
+    if (emp){
+      this.getImportaciones(Number(emp))
+    }
+  }
+
+  getImportaciones(empresa: number){
+    this.comimpService.getImportacionPen(empresa).subscribe({
+      next: data => {
+        console.log(data)
+        this.listImportaciones = data
+      }
+    })
+  }
+
+  onGlobalFilter(table: Table, event: Event) {
+    table.filterGlobal((event.target as HTMLInputElement).value, 'contains')
+  }
+
+  selectedImp(doc: ComImpV1){
+    this.docSelected = doc
+    this.imporSelected = true;
+  }
 }
