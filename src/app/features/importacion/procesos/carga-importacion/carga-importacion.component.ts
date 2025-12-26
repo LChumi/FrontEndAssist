3ea3457aefdcb94eb649ec801204ff0733e5a4e2
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-12-26 12:29:30 -0500

    feat: added new module

---
+++ src/app/features/importacion/procesos/carga-importacion/carga-importacion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './carga-importacion.component.html',
+  styles: ``
+})
+export class CargaImportacionComponent {
+
+}
