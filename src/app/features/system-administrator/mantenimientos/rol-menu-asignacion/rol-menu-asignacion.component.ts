49d5cfd6b06ff29610ce65986ff670b669660bf8
LChumi <luischumi.9@gmail.com>
2026-07-10 16:50:58 -0500

    feat: added new components and added process to menu save edit etc (#6)

---
+++ src/app/features/system-administrator/mantenimientos/rol-menu-asignacion/rol-menu-asignacion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rol-menu-asignacion',
+  standalone: true,
+  imports: [],
+  templateUrl: './rol-menu-asignacion.component.html',
+  styles: ``
+})
+export class RolMenuAsignacionComponent {
+
+}
