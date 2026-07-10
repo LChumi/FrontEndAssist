5fecfabeb8d53d498a66390b4a483e6c4771f7e4
LChumi <luischumi.9@gmail.com>
2026-07-10 13:01:07 -0500

    feat: added new interfaces and update services (#6)

---
+++ src/app/core/services/api/assist/rol-w.service.ts
@@ -0,0 +1,31 @@
+import {inject, Injectable} from '@angular/core';
+import {RolW} from "@models/entities/rol-w";
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "@environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RolWService {
+
+  private baseUrl = `${environment.apiUrlBase}/assist`
+  private http = inject(HttpClient)
+
+  getAll():Observable<RolW[]>{
+    return this.http.get<RolW[]>(`${this.baseUrl}/menuw/all`)
+  }
+
+  getById(id: number):Observable<RolW>{
+    return this.http.get<RolW>(`${this.baseUrl}/menuw/${id}`)
+  }
+
+  create(menu:RolW): Observable<RolW>{
+    return this.http.post<RolW>(`${this.baseUrl}/menuw`,menu)
+  }
+
+  update(menu:RolW): Observable<RolW>{
+    return this.http.put<RolW>(`${this.baseUrl}/menuw`,menu)
+  }
+
+}
