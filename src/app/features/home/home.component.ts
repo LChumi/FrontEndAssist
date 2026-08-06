55cc8420964451d5ae08f68984320c2163ae0ae3
LChumi <luischumi.9@gmail.com>
2026-08-06 12:00:55 -0500

    feat: added seo service to pages

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {ButtonDirective} from "primeng/button";
 import {RouterLink} from "@angular/router";
 import {NgOptimizedImage} from "@angular/common";
+import {SeoHelperService} from "@services/state/seo-helper.service";
 
 @Component({
   selector: 'app-home',
@@ -14,7 +15,17 @@ import {NgOptimizedImage} from "@angular/common";
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  private seoHelper = inject(SeoHelperService)
+
+  ngOnInit(): void {
+    this.seoHelper.setupPageSeo({
+      title: 'Pagina de Inicio | Assist Web',
+      description: 'Pagina de Inicio para procesos internos del sistema Assist Web',
+      schemaTitle: 'ContentPage'
+    });
+  }
 
   protected readonly email = "lchumi@cumpleanos.com.ec - luischumi.9@gmail.com"
 
